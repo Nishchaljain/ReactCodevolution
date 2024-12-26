be2874da4104ab22d8f8a450715764ced036b98f
Nishchaljain <nishchaljain786@gmail.com>
2024-12-26 22:36:54 +0530

    much more cleaner way of rendering a list

--- src/components/NameList.jsx
+++ src/components/NameList.jsx
@@ -2,13 +2,8 @@ import React from "react";
 
 const NameList = () => {
   const names = ["Nishchal", "Divya", "Samriddhi", "Ridit"];
-  return (
-    <div>
-      {names.map((name) => (
-        <h2>{name}</h2>
-      ))}
-    </div>
-  );
+  const nameList = names.map((name) => <h2>{name}</h2>);
+  return <div> {nameList}</div>;
 };
 
 export default NameList;
