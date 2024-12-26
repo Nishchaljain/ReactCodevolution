c5c29cef6721f00ce36bb5c407635ff30e76bb09
Nishchaljain <nishchaljain786@gmail.com>
2024-12-26 22:16:55 +0530

    conditional rendering using ternory operator

--- src/components/UserGreeting.jsx
+++ src/components/UserGreeting.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 
 const UserGreeting = () => {
-  const [IsloggedIn, setIsLoggedIn] = useState(false);
+  const [IsloggedIn, setIsLoggedIn] = useState(true);
   //   if (IsloggedIn) {
   //     return <div>Welcome Nishchal</div>;
   //   } else {
   //     return <div>Welcome Guest</div>;
   //   }
-  let message;
-  if (IsloggedIn) {
-    message = <div>Welcome Nishchal</div>;
-  } else {
-    message = <div>Welcome Guest</div>;
-  }
-  return <div>{message}</div>;
+  //   let message;
+  //   if (IsloggedIn) {
+  //     message = <div>Welcome Nishchal</div>;
+  //   } else {
+  //     message = <div>Welcome Guest</div>;
+  //   }
+  //   return <div>{message}</div>;
+  return IsloggedIn ? <div>Welcome Nishchal</div> : <div>Welcome Guest</div>;
 };
 
 export default UserGreeting;
